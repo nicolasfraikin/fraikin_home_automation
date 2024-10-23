07d397ed9b07c6f6c3bcea6f29e43520c6f709be
Nicolas Fraikin <nicolas.fraikin3@gmail.com>
2024-10-23 17:04:02 +0200

    Add simple android app

---
+++ android_app/src/main/java/example/hello_world/Greeter.java
@@ -0,0 +1,10 @@
+package example.hello_world;
+
+/**
+ * A tiny Greeter library for the Bazel Android "Hello, World" app.
+ */
+public class Greeter {
+  public String sayHello() {
+    return "Hello Friend! \uD83D\uDC4B\uD83C\uDF31"; // Unicode for 👋🌱
+  }
+}
